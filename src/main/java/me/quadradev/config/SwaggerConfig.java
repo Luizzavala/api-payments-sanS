b1a993751c401b3772739db05cedf92c9be2f901
Luis Zavala <94495979+Luizzavala@users.noreply.github.com>
2025-08-07 16:41:16 -0700

    docs: add jwt security to swagger

--- src/main/java/me/quadradev/config/SwaggerConfig.java
+++ src/main/java/me/quadradev/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package me.quadradev.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,13 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI api() {
-        return new OpenAPI().info(new Info().title("API").version("1.0"));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info().title("API").version("1.0"));
     }
 }
