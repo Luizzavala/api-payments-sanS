0e6e5e451f02d1ba2cab1b71f86339d4b75b208b
LuisZavala <luisbancaprepa@gmail.com>
2025-08-07 11:31:16 -0700

    hide password only write

--- src/main/java/me/quadradev/application/core/model/User.java
+++ src/main/java/me/quadradev/application/core/model/User.java
@@ -1,5 +1,7 @@
 package me.quadradev.application.core.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,6 +20,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private String email;
