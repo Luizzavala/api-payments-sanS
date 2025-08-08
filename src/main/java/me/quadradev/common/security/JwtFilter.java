e6e3675c66ed9bdb8915ac2dd1e089d67f6b8cac
LuisZavala <luisbancaprepa@gmail.com>
2025-08-07 18:15:39 -0700

    api/auth/** public enpoints into dofilter

--- src/main/java/me/quadradev/common/security/JwtFilter.java
+++ src/main/java/me/quadradev/common/security/JwtFilter.java
@@ -26,6 +26,12 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+        if (path.startsWith("/api/auth/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
